2900cf427ee9efd1ee8873822430488ce11e7f76
mrfrederico-ist <mrfrederico89@gmail.com>
2017-04-05 12:06:51 +0100

    Create app Models

--- Web/pomodorosite/pomodoro/models.py
+++ Web/pomodorosite/pomodoro/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    completed = models.BooleanField()
+    finished_date = models.DateField()
+
+
+class Pomodoro(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    duration = models.IntegerField()
