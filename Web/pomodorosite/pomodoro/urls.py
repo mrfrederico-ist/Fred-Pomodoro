9c6c3d1f3089017806c87150d433e28117b9824f
mrfrederico-ist <mrfrederico89@gmail.com>
2017-04-09 13:50:31 +0100

    Add skeleton views, Add base templates and skeleton templates

--- Web/pomodorosite/pomodoro/urls.py
+++ Web/pomodorosite/pomodoro/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^tasks/$', views.tasks, name='tasks'),
+    url(r'^settings/$', views.settings, name='settings'),
 ]
--- Web/pomodorosite/pomodoro/views.py
+++ Web/pomodorosite/pomodoro/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 
 def index(request):
-    """
-    Display Pomodoro home page.
-     **Template**
-     :template:`pomodoro/index.html`
-    """
     return render(request, 'pomodoro/index.html')
+
+
+def tasks(request):
+    return render(request, 'pomodoro/tasks.html')
+
+
+def settings(request):
+    return render(request, 'pomodoro/settings.html')
